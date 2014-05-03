583e2a698d2ed4bebeefc3a15985714b3374e8a4
Willson Mock <willson.mock@gmail.com>
2014-05-03 11:46:30 -0700

    Add instructions

--- app/src/views/reflowableScrollview.js
+++ app/src/views/reflowableScrollview.js
@@ -11,6 +11,18 @@ define(function(require, exports, module) {
      * @description
      */
 
+/*
+
+1) Create a reflowing / autosizing scrollview from http://codepen.io/befamous/pen/kbxnH
+2) Before making a reusable widget, explore coding this as a view by whatever method seems 
+most natural to you. You will need to write some code that takes the views' size every frame
+(either through context.size returned to you in the commit function or hacking it up with
+window.innerWidth/Height) and then creating subviews that are packed correctly for
+the width/height.
+3) There's value in a naive first pass at implementing the scrollview.
+
+*/
+
     function reflowableScrollview() {
         View.apply(this, arguments);
     }
