5b4ef0b72f46ba59c0dc159b7c00e8e660e035ac
gregorylull <gregorylull@gmail.com>
2014-05-07 14:24:04 -0700

    working on animation bug

--- app/src/main.js
+++ app/src/main.js
@@ -17,7 +17,7 @@ define(function(require, exports, module) {
     var logos = [];
     // var famousLogo;
     // var hackreactorLogo;
-    var num = 100;
+    var num = 50;
     var sizeCounter = 1;
     for (var i = 0; i < num; i += 1) {
         // hackreactorLogo = new ImageSurface({
@@ -51,4 +51,70 @@ define(function(require, exports, module) {
     window.reflowable = reflowable;
 
     mainContext.add(reflowable);
+
+    // /////////// greg test ///////////////
+    // // greg test: //
+    // var View = require('famous/core/View');
+    // var StateModifier = require('famous/modifiers/StateModifier');
+    // var TransitionableTransform = require('famous/transitions/TransitionableTransform');
+    // var Scrollview = require('famous/views/Scrollview');
+    
+    // // view gets translated to a certain place
+    // var v1 = new View();
+    // var v2 = new View();
+
+    // var s1 = new Surface({
+    //     size: [100, 100],
+    //     properties: {backgroundColor: 'red'}
+    // });
+    // var s2 = new Surface({
+    //     size: [100, 100],
+    //     properties: {backgroundColor: 'blue'}
+    // });
+
+    // window.s1 = s1;
+    // window.s2 = s2;
+    // window.Transform = Transform;
+
+    // // // mimic inner render AKA downstream
+    // var m1InnerRender = new Modifier({
+    //     transform: Transform.translate(0, 200, 0)
+    // });
+
+    // // // mimic our Transitionable function. UPSTREAM
+    // var customUpstream = new Modifier({
+    //     transform: customFunction
+    //     // transform: Transform.translate(0, -200, 0)
+
+    // });
+
+    // v1.add(customUpstream).add(s2);          // custom     
+    
+
+    // var tt1 = new TransitionableTransform();
+    // // var callback = function () {
+    // //     console.log('execute');
+    // // };
+
+    // tt1.setTranslate([0, 200, 0], {duration: 3000, curve: 'easeInOut'});
+
+    // function customFunction () {
+    //     var current = tt1.get();
+    //     var translateOffset = Transform.translate(0, -200, 0);
+    //     return Transform.multiply(current, translateOffset);
+        
+    // };
+
+    // // s2.on('click', function () {
+    // //     tt1.halt();
+    // //     var orig = tt1.get();
+    // //     var trans = [300, 0, 0];
+    // //     var newPos = Transform.translate.apply(null, trans);
+    // //     var combined = Transform.multiply(orig, newPos);
+    // //     tt1.set(combined, {duration: 1000, curve: 'easeInOut'});
+    // // });
+
+
+    // mainContext.add(m1InnerRender).add(v1);  // innerREnder, downstream
+    // //////// greg END test/////////////
 });
